bcd61a9b25fb52a52f61bf06c1ddbb4ea562665d
spring <spring@DESKTOP-GV5BU8H>
2025-04-26 09:01:06 +0900

    プロジェクト ファイルを追加します。

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            comboBox1 = new ComboBox();
+            panel2 = new Panel();
+            comboBox2 = new ComboBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AllowDrop = true;
+            panel1.Controls.Add(comboBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 361);
+            panel1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(3, 326);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(194, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.AllowDrop = true;
+            panel2.Controls.Add(comboBox2);
+            panel2.Dock = DockStyle.Right;
+            panel2.Location = new Point(204, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 361);
+            panel2.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(6, 326);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(194, 23);
+            comboBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(404, 361);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private ComboBox comboBox1;
+        private Panel panel2;
+        private ComboBox comboBox2;
+    }
+}
